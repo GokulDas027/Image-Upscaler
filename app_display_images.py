6f855db2914868e0fd11c4c375afc3f9f4a377c3
Gokuldas K M <gokuldas027@gmail.com>
2020-06-18 00:36:16 +0530

    eval for comparing True and "True"

--- app_display_images.py
+++ app_display_images.py
@@ -39,15 +39,10 @@ def upload():
     gan = request.form.get('mycheckbox')
     scale = request.form.get('mycheckbox1')
     resl = request.form.get('mycheckbox2')
-
-    gan = True if gan == "True" else False
-    resl = True if resl == "True" else False
-
     print(gan)
     print(scale)
     print(resl)
-
-    flask_return = image_upscaler.main(scale=int(scale), gan=gan, keep_res=resl)
+    flask_return = image_upscaler.main(scale=int(scale), gan=eval(gan), keep_res=eval(resl))
     return render_template("display.html", image_name=files_list)
 
 @app.route('/upload/<filename>')
